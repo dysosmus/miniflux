a79a63db1bd2ccaa23a857eab882f4673d648dbd
Frederic Guillot <contact@fredericguillot.com>
2013-03-20 00:21:20 -0400

    Remove useless css class

--- miniflux/templates/config.php
+++ miniflux/templates/config.php
@@ -4,13 +4,13 @@
 
 <form method="post" action="?action=config">
 
-    <?= Helper\form_label('Username', 'username', 'control-label') ?>
+    <?= Helper\form_label('Username', 'username') ?>
     <?= Helper\form_text('username', $values, $errors, array('required')) ?><br/>
 
-    <?= Helper\form_label('Password', 'password', 'control-label') ?>
+    <?= Helper\form_label('Password', 'password') ?>
     <?= Helper\form_password('password', $values, $errors) ?><br/>
 
-    <?= Helper\form_label('Confirmation', 'confirmation', 'control-label') ?>
+    <?= Helper\form_label('Confirmation', 'confirmation') ?>
     <?= Helper\form_password('confirmation', $values, $errors) ?><br/>
 
     <div class="form-actions">
