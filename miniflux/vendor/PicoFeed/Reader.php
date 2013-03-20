d9a47f71d6c98a8bebdcd1af5a1b9459b8251bad
Frederic Guillot <contact@fredericguillot.com>
2013-03-20 00:19:12 -0400

    Update of PicoFeed

--- miniflux/vendor/PicoFeed/Reader.php
+++ miniflux/vendor/PicoFeed/Reader.php
@@ -70,6 +70,10 @@ class Reader
 
             return new Rss20($this->content);
         }
+        else if (strpos($first_tag, '<rdf:') !== false && strpos($first_tag, 'xmlns="http://purl.org/rss/1.0/"') !== false) {
+
+            return new Rss10($this->content);
+        }
         else if ($discover === true) {
 
             return false;
