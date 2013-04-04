d020b9f2d9a04de6ea9bf1764653d116df3c768c
Frederic Guillot <contact@fredericguillot.com>
2013-04-03 21:18:30 -0400

    Try to do a better preferences page

--- miniflux/templates/config.php
+++ miniflux/templates/config.php
@@ -1,5 +1,5 @@
 <div class="page-header">
-    <h2>Username and password</h2>
+    <h2>Change username and password</h2>
 </div>
 <section>
 <form method="post" action="?action=config" autocomplete="off">
@@ -21,37 +21,31 @@
 </section>
 
 <div class="page-section">
-    <h2>My data</h2>
+    <h2>More informations</h2>
 </div>
 <section>
-<div class="alert alert-normal">
-    <h3>Database</h3>
-    <ul>
-        <li>Database size: <?= Helper\format_bytes($db_size) ?></li>
-        <li><a href="?action=optimize-db">Optimize the database</a> (VACUUM command)</li>
-        <li><a href="?action=download-db">Download the entire database</a> (Gzip compressed Sqlite file).</li>
-    </ul>
-</div>
-</section>
-
-<div class="page-section">
-    <h2>Help</h2>
-</div>
-<section>
-<div class="alert">
-    <h3>Keyboard shortcuts</h3>
-    <ul>
-        <li>Previous item = <strong>p</strong></li>
-        <li>Next item = <strong>n</strong></li>
-        <li>Mark as read or unread = <strong>m</strong></li>
-        <li>Open original link = <strong>v</strong></li>
-        <li>Open item = <strong>o</strong></li>
-    </ul>
-</div>
-<div class="alert alert-info">
-    <h3>About</h3>
-    <ul>
-        <li>Miniflux version: <strong><?= APP_VERSION ?></strong></li>
-    </ul>
-</div>
+    <div class="alert alert-normal">
+        <h3>Database</h3>
+        <ul>
+            <li>Database size: <?= Helper\format_bytes($db_size) ?></li>
+            <li><a href="?action=optimize-db">Optimize the database</a> (VACUUM command)</li>
+            <li><a href="?action=download-db">Download the entire database</a> (Gzip compressed Sqlite file).</li>
+        </ul>
+    </div>
+    <div class="alert alert-normal">
+        <h3>Keyboard shortcuts</h3>
+        <ul>
+            <li>Previous item = <strong>p</strong></li>
+            <li>Next item = <strong>n</strong></li>
+            <li>Mark as read or unread = <strong>m</strong></li>
+            <li>Open original link = <strong>v</strong></li>
+            <li>Open item = <strong>o</strong></li>
+        </ul>
+    </div>
+    <div class="alert alert-normal">
+        <h3>About</h3>
+        <ul>
+            <li>Miniflux version: <strong><?= APP_VERSION ?></strong></li>
+        </ul>
+    </div>
 </section>
